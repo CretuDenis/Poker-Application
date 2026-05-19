55495a2cf7fec89de46427dde888f645da72e92f
Cretu Denis <cretu.denis16@gmail.com>
2026-05-19 17:31:13 +0300

    fixed backend blocking frontend in prod

--- backend/src/main/java/com/example/Poker/config/SecurityConfig.java
+++ backend/src/main/java/com/example/Poker/config/SecurityConfig.java
@@ -57,7 +57,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173")); // to be changed in prod
+        config.setAllowedOrigins(List.of("https://poker-frontend-kzrl.onrender.com")); // to be changed in prod
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
